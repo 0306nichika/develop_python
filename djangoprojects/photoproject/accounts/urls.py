cbbbeb2e3f741dbb862189d118f54c8dddf2b6a5
narikiyo3613 <n.nichika0306@gmail.com>
2026-06-22 12:09:42 +0900

    Create urls.py

---
+++ djangoprojects/photoproject/accounts/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path
+# viewsモジュールをインポート
+from . import views
+# URLパターンを逆引きできるように名前を付ける
+app_name = 'accounts'
+
+# URLパターンを登録するための変数
+urlpatterns = [
+    # サインアップページのビューを呼び出し
+    # [http(s)://<ホスト名>/signup/]へのアクセスに対し
+    # viewsモジュールのSignUpViewsをインスタンス化する
+    path('signup/',
+         views.SignUpView.as_view(),
+         name = 'signup',
+    )
+    
+    #サインアップ完了ページのビューを呼び出し
+    # [http(s)://<ホスト名>/signup_success/]へのアクセスに対し
+    # viewsモジュールのSignUpSuccessViewsをインスタンス化する
+    path('signup_success/',
+         views.SignUpSuccessView.as_view(),
+         name = 'signup_success',
+    )
+]
+
