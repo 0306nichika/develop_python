1be0aef4412cc1c33a7e78f9612f04f7d041af4e
nao1019yuki <naoyuki_inoue@totec.co.jp>
2026-06-23 15:03:48 +0900

    add

--- djangoprojects/photoproject/photoproject/urls.py
+++ djangoprojects/photoproject/photoproject/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+#auth.viewをインポートしてauth_viewという名前で利用する
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +26,25 @@ urlpatterns = [
     
     # accounts.urlへのURLパターン
     path('',include('accounts.urls')),
+
+
+path('password_reset/',
+     auth_views.PasswordResetView.as_view(
+         template_name = "password_reset.html"),
+    name = 'password_reset'),
+
+path('password_reset/done/',
+     auth_views.PasswordResetDoneView.as_view(
+         template_name = "password_reset_sent.html"),
+    name = 'password_reset_done'),
+
+path('reset/<uidb64>/<token>',
+     auth_views.PasswordResetConfirmView.as_view(
+         template_name = "password_reset_form.html"),
+    name = 'password_reset_confirm'),
+
+path('reset/done/',
+     auth_views.PasswordResetCompleteView.as_view(
+         template_name = "password_reset_done.html"),
+    name = 'password_reset_complete'),
 ]
