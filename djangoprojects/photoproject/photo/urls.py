a91938e22b9140916aeb2638f5b92d2e944a56f1
narikiyo3613 <n.nichika0306@gmail.com>
2026-06-24 16:24:51 +0900

    add

--- djangoprojects/photoproject/photo/urls.py
+++ djangoprojects/photoproject/photo/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from .views import like
+from django.views.generic import DeleteView
 
 
 #URLパターンを逆引きできるように名前を付ける
@@ -17,5 +18,6 @@ urlpatterns = [
     path('photos/<int:category>/',views.CategoryView.as_view(),name = 'photos_映画ポスター'),
     
     path('like/', views.like, name='like'),
+    path('photo/<int:pk>/delete',views.PhotoDeleteView(),name = 'photo_delete'),
 
 ]
